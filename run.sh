8e4de7889dc5a17e108def15da5554afcaed2409
zeitmanl <zeitmanl@gmail.com>
2024-01-24 12:36:14 -0800

    Script to run the app

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java --module-path ./javafx-sdk-21/lib --add-modules javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web -jar bouncingballs.jar
